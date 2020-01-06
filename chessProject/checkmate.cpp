0767a90399ba14ac39ad15117ee44b1d0131cbf0
Shahar Tefler <shahartefler@gmail.com>
2020-01-06 18:51:06 +0200

    fixed checkmate

--- chessProject/checkmate.cpp
+++ chessProject/checkmate.cpp
@@ -13,18 +13,21 @@ CODES checkmate::isCheckmate(Board& board, King* otherKing, Piece* attacker)
 	}
 	std::string kingLocation = otherKing->getCurrLocation(), attackerLocation = attacker->getCurrLocation(), squareBetween = kingLocation, move = "";
 	bool isEscapingPath = false, canAttackerBeEated = false;
-	
+	board.setCurrPlayer(!board.getCurrPlayer());
+
 	/*Checking for a valid move to escape the attacker (checking if there's a valid King move for all possible destinations).*/
 	isEscapingPath = 
-		NO_PIECE_IN_SRC > board.isMoveValid(kingLocation + ((char)(kingLocation[COL] + 1)) + kingLocation[ROW]) &&
-		NO_PIECE_IN_SRC > board.isMoveValid(kingLocation + ((char)(kingLocation[COL] - 1)) + kingLocation[ROW]) &&
-		NO_PIECE_IN_SRC > board.isMoveValid(kingLocation + ((char)(kingLocation[COL] + 1)) + ((char)(kingLocation[ROW] + 1))) &&
-		NO_PIECE_IN_SRC > board.isMoveValid(kingLocation + ((char)(kingLocation[COL] + 1)) + ((char)(kingLocation[ROW] - 1))) &&
-		NO_PIECE_IN_SRC > board.isMoveValid(kingLocation + ((char)(kingLocation[COL] - 1)) + ((char)(kingLocation[ROW] - 1))) &&
-		NO_PIECE_IN_SRC > board.isMoveValid(kingLocation + ((char)(kingLocation[COL] - 1)) + ((char)(kingLocation[ROW] + 1))) &&
-		NO_PIECE_IN_SRC > board.isMoveValid(kingLocation + kingLocation[COL] + ((char)(kingLocation[ROW] - 1))) &&
-		NO_PIECE_IN_SRC > board.isMoveValid(kingLocation + kingLocation[COL] + ((char)(kingLocation[ROW] - 1)));
+		NO_PIECE_IN_SRC > board.isMoveValid(kingLocation + ((char)(kingLocation[SRC_COL] + 1)) + kingLocation[SRC_ROW]) ||
+		NO_PIECE_IN_SRC > board.isMoveValid(kingLocation + ((char)(kingLocation[SRC_COL] - 1)) + kingLocation[SRC_ROW]) ||
+		NO_PIECE_IN_SRC > board.isMoveValid(kingLocation + ((char)(kingLocation[SRC_COL] + 1)) + ((char)(kingLocation[SRC_ROW] + 1))) ||
+		NO_PIECE_IN_SRC > board.isMoveValid(kingLocation + ((char)(kingLocation[SRC_COL] + 1)) + ((char)(kingLocation[SRC_ROW] - 1))) ||
+		NO_PIECE_IN_SRC > board.isMoveValid(kingLocation + ((char)(kingLocation[SRC_COL] - 1)) + ((char)(kingLocation[SRC_ROW] - 1))) ||
+		NO_PIECE_IN_SRC > board.isMoveValid(kingLocation + ((char)(kingLocation[SRC_COL] - 1)) + ((char)(kingLocation[SRC_ROW] + 1))) ||
+		NO_PIECE_IN_SRC > board.isMoveValid(kingLocation + kingLocation[SRC_COL] + ((char)(kingLocation[SRC_ROW] - 1))) ||
+		NO_PIECE_IN_SRC > board.isMoveValid(kingLocation + kingLocation[SRC_COL] + ((char)(kingLocation[SRC_ROW] - 1)));
 
+	board.setCurrPlayer(!board.getCurrPlayer());
+	std::cout << isEscapingPath << std::endl;
 	/*Checking if the attacker can be eaten.*/
 	board.setCurrPlayer(!board.getCurrPlayer()); // Check moves of the oppenet.
 	for (int i = 0; (i < board.getPieces().size()) && !canAttackerBeEated; i++)
